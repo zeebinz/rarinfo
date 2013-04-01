8fb480a6ef457743986d10e1aeba5caa200977c8
zeebinz <zeebinz@gmail.com>
2013-04-01 14:47:48 +0100

    Updates generate.php with command options
    It should now only generate missing fixtures by default unless the
    -r switch is used to refresh everything.

--- tests/fixtures/generate.php
+++ tests/fixtures/generate.php
@@ -1,39 +1,52 @@
 <?php
 /**
- * Script to generate test fixture files, should be run before starting tests
- * and again after each update.
+ * Script to generate test fixture files. This should be run before starting tests
+ * and again after each update with the -r option to regenerate.
+ *
+ * Command options:
+ *  -t <type>  The type of fixture to generate: rar, srr, par2, zip (default is all)
+ *  -r         Regenerate existing fixture files (default is to create only missing ones)
+ *  -p         Run in pretend mode, output debug info without making changes
  */
-error_reporting(E_ALL);
+error_reporting(E_ALL | E_STRICT);
 ini_set('display_startup_errors', 'on');
 ini_set('display_errors', 'on');
 
-makeRarFixtures();
-makeSrrFixtures();
-makePar2Fixtures();
-makeZipFixtures();
+$opts = getopt('prt:');
+$pretend = isset($opts['p']) ? true : false;
+$refresh = isset($opts['r']) ? true : false;
+
+if ($pretend) {echo "*** Running in pretend mode ***\n";}
+if (!isset($opts['t']) || $opts['t'] == 'rar')  makeRarFixtures($pretend, $refresh);
+if (!isset($opts['t']) || $opts['t'] == 'srr')  makeSrrFixtures($pretend, $refresh);
+if (!isset($opts['t']) || $opts['t'] == 'par2') makePar2Fixtures($pretend, $refresh);
+if (!isset($opts['t']) || $opts['t'] == 'zip')  makeZipFixtures($pretend, $refresh);
 
 /**
  * Generates test fixtures from RAR sample files.
  *
  * @param   boolean  $pretend  debug output only?
+ * @param   boolean  $refresh  regenerate existing files?
  * @return  void
  */
-function makeRarFixtures($pretend=false)
+function makeRarFixtures($pretend=false, $refresh=true)
 {
 	require_once dirname(__FILE__).'/../../rarinfo.php';
 	$rar = new RarInfo;
 	foreach(glob(dirname(__FILE__).'/rar/*.rar') as $rarfile) {
+		$fname = pathinfo($rarfile, PATHINFO_BASENAME).'.blocks';
+		$file  = dirname(__FILE__)."/rar/$fname";
+		if (!$refresh && file_exists($file)) {continue;}
 		echo "Generating for $rarfile:\n";
 		$rar->open($rarfile, true);
 		if ($rar->error) {
 			echo "Error: {$rar->error}\n";
 			continue;
 		}
-		$fname = pathinfo($rarfile, PATHINFO_BASENAME).'.blocks';
 		$data = $rar->getBlocks();
 		if (!$pretend) {
 			$data = "<?php\nreturn ".var_export($data, true).";\n";
-			file_put_contents(dirname(__FILE__)."/rar/$fname", $data);
+			file_put_contents($file, $data);
 		}
 		echo "-- $fname\n";
 	}
@@ -43,24 +56,27 @@ function makeRarFixtures($pretend=false)
  * Generates test fixtures from SRR sample files.
  *
  * @param   boolean  $pretend  debug output only?
+ * @param   boolean  $refresh  regenerate existing files?
  * @return  void
  */
-function makeSrrFixtures($pretend=false)
+function makeSrrFixtures($pretend=false, $refresh=true)
 {
 	require_once dirname(__FILE__).'/../../srrinfo.php';
 	$srr = new SrrInfo;
 	foreach(glob(dirname(__FILE__).'/srr/*.srr') as $srrfile) {
+		$fname = pathinfo($srrfile, PATHINFO_BASENAME).'.blocks';
+		$file  = dirname(__FILE__)."/srr/$fname";
+		if (!$refresh && file_exists($file)) {continue;}
 		echo "Generating for $srrfile:\n";
 		$srr->open($srrfile);
 		if ($srr->error) {
 			echo "Error: {$srr->error}\n";
 			continue;
 		}
-		$fname = pathinfo($srrfile, PATHINFO_BASENAME).'.blocks';
 		$data = $srr->getBlocks();
 		if (!$pretend) {
 			$data = "<?php\nreturn ".var_export($data, true).";\n";
-			file_put_contents(dirname(__FILE__)."/srr/$fname", $data);
+			file_put_contents($file, $data);
 		}
 		echo "-- $fname\n";
 	}
@@ -70,24 +86,27 @@ function makeSrrFixtures($pretend=false)
  * Generates test fixtures from PAR2 sample files.
  *
  * @param   boolean  $pretend  debug output only?
+ * @param   boolean  $refresh  regenerate existing files?
  * @return  void
  */
-function makePar2Fixtures($pretend=false)
+function makePar2Fixtures($pretend=false, $refresh=true)
 {
 	require_once dirname(__FILE__).'/../../par2info.php';
 	$par2 = new Par2Info;
 	foreach(glob(dirname(__FILE__).'/par2/*.par2') as $par2file) {
+		$fname = pathinfo($par2file, PATHINFO_BASENAME).'.packets';
+		$file  = dirname(__FILE__)."/par2/$fname";
+		if (!$refresh && file_exists($file)) {continue;}
 		echo "Generating for $par2file:\n";
 		$par2->open($par2file);
 		if ($par2->error) {
 			echo "Error: {$par2->error}\n";
 			continue;
 		}
-		$fname = pathinfo($par2file, PATHINFO_BASENAME).'.packets';
 		$data = $par2->getPackets(true);
 		if (!$pretend) {
 			$data = "<?php\nreturn ".var_export($data, true).";\n";
-			file_put_contents(dirname(__FILE__)."/par2/$fname", $data);
+			file_put_contents($file, $data);
 		}
 		echo "-- $fname\n";
 	}
@@ -97,24 +116,27 @@ function makePar2Fixtures($pretend=false)
  * Generates test fixtures from ZIP sample files.
  *
  * @param   boolean  $pretend  debug output only?
+ * @param   boolean  $refresh  regenerate existing files?
  * @return  void
  */
-function makeZipFixtures($pretend=false)
+function makeZipFixtures($pretend=false, $refresh=true)
 {
 	require_once dirname(__FILE__).'/../../zipinfo.php';
-	$zip = new zipInfo;
+	$zip = new ZipInfo;
 	foreach(glob(dirname(__FILE__).'/zip/*.zip') as $zipfile) {
+		$fname = pathinfo($zipfile, PATHINFO_BASENAME).'.records';
+		$file  = dirname(__FILE__)."/zip/$fname";
+		if (!$refresh && file_exists($file)) {continue;}
 		echo "Generating for $zipfile:\n";
 		$zip->open($zipfile);
 		if ($zip->error) {
 			echo "Error: {$zip->error}\n";
 			continue;
 		}
-		$fname = pathinfo($zipfile, PATHINFO_BASENAME).'.records';
 		$data = $zip->getRecords();
 		if (!$pretend) {
 			$data = "<?php\nreturn ".var_export($data, true).";\n";
-			file_put_contents(dirname(__FILE__)."/zip/$fname", $data);
+			file_put_contents($file, $data);
 		}
 		echo "-- $fname\n";
 	}
